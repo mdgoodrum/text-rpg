7d2d9a56622c395771599e1d539065c2c6dc6a60
Joel <joelgoodrum@hotmail.com>
2018-09-01 20:07:27 -0400

    Add music studio room

---
+++ StudioRoom.js
@@ -0,0 +1,10 @@
+//code logic for 
+
+function goToStudio() {
+	var img = document.getElementById("locationImage");
+	img.src ="theStudio Img.png";
+	characterLocation = "Welcome to the studio.";
+	updateGame();
+}
+
+
